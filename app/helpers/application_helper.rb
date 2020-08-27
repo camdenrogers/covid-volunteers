249e7ec4815e871ba020b7529e89cead5daf88fe
Camden Rogers <camdenrogers@att.net>
2020-08-27 17:34:11 -0400

    Started updating nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,20 +22,47 @@ module ApplicationHelper
     end
   end
 
+  def nav_submenu_link_active_class(variant = 'DESKTOP')
+    case variant
+    when 'DESKTOP'
+      'border-' + @primary_color + '-500 ' + 'focus:border-' + @primary_color + '-700 ' + 'inline-flex items-center px-1 pt-1 border-b-2 text-sm leading-5 text-gray-900 focus:outline-none transition duration-150 ease-in-out ml-4 text-center'
+    when 'MOBILE'
+      'border-' + @primary_color + '-500 ' + 'focus:border-' + @primary_color + '-700 ' + 'text-' + @primary_color + '-700 ' + 'bg-' + @primary_color + '-50 ' + 'focus:text-' + @primary_color + '-800 ' + 'focus:bg-' + @primary_color + '-100 ' + 'block pl-3 pr-4 py-2 border-l-4 text-base focus:outline-none transition duration-150 ease-in-out'
+    end
+  end
+
+  def nav_submenu_link_inactive_class(variant = 'DESKTOP')
+    case variant
+    when 'DESKTOP'
+    'block px-4 py-2 text-gray-600 hover:bg-gray-50 hover:text-gray-800'
+    when 'MOBILE'
+    'mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out'
+    end
+  end
+
+  def about_nav_link_class(variant = 'DESKTOP')
+    current_page?(about_path) ? nav_link_active_class(variant) : nav_link_inactive_class(variant)
+  end
+
   def all_projects_nav_link_class(variant = 'DESKTOP')
     params[:controller] == 'projects' && params[:action] == 'index' ? nav_link_active_class(variant) : nav_link_inactive_class(variant)
   end
 
   def volunteered_projects_nav_link_class(variant = 'DESKTOP')
-    params[:controller] == 'projects' && params[:action] == 'volunteered' ? nav_link_active_class(variant) : nav_link_inactive_class(variant)
+    # params[:controller] == 'projects' && params[:action] == 'volunteered' ? nav_link_active_class(variant) : nav_link_inactive_class(variant)
+    params[:controller] == 'projects' && params[:action] == 'volunteered' ? nav_submenu_link_active_class(variant) : nav_submenu_link_inactive_class(variant)
+
   end
 
   def own_projects_nav_link_class(variant = 'DESKTOP')
-    params[:controller] == 'projects' && params[:action] == 'own' ? nav_link_active_class(variant) : nav_link_inactive_class(variant)
+    #params[:controller] == 'projects' && params[:action] == 'own' ? nav_link_active_class(variant) : nav_link_inactive_class(variant)
+    params[:controller] == 'projects' && params[:action] == 'own' ? nav_submenu_link_active_class(variant) : nav_submenu_link_inactive_class(variant)
+
   end
 
   def profile_nav_link_class(variant = 'DESKTOP')
-    params[:controller] == 'users/registrations' && [ 'edit', 'update' ].include?(params[:action]) ? nav_link_active_class(variant) : nav_link_inactive_class(variant)
+    #params[:controller] == 'users/registrations' && [ 'edit', 'update' ].include?(params[:action]) ? nav_link_active_class(variant) : nav_link_inactive_class(variant)
+    params[:controller] == 'users/registrations' && [ 'edit', 'update' ].include?(params[:action]) ? nav_submenu_link_active_class(variant) : nav_submenu_link_inactive_class(variant)
   end
 
   def sign_up_nav_link_class(variant = 'DESKTOP')
@@ -55,7 +82,9 @@ module ApplicationHelper
   end
 
   def logout_nav_link_class(variant = 'DESKTOP')
-    nav_link_inactive_class(variant)
+    #nav_link_inactive_class(variant)
+    nav_submenu_link_inactive_class(variant)
+
   end
 
   def discord_nav_link_class(variant = 'DESKTOP')
